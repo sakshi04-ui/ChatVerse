2b9d93d590e78b6bb7c66cb17fba3928c5ae06e3
Sakshi Jain <jainsakshi0408@gmail.com>
2025-05-11 10:02:23 +0530

    Add files via upload

---
+++ my-app/components/app-sidebar.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import {  MessagesSquare } from "lucide-react"
+import Link from "next/link"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { ThreadList } from "./assistant-ui/thread-list"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+                <Link href="https://github.com/sakshi04-ui/ChatVerse/tree/main" target="_blank">
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <MessagesSquare className="size-4" />
+                  </div>
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="font-semibold">ChatVerse</span>
+                  </div>
+                </Link>
+              </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <ThreadList />
+      </SidebarContent>
+      
+      <SidebarRail />
+      <SidebarFooter>
+        <SidebarMenu>
+         
+          
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
